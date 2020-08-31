f7622c33c3405459ea0f84e7196d8359dcc2aadf
Christopher Henderson <chenderson5280@gmail.com>
2020-08-31 21:15:13 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s" do
+  if string[0] == "s" 
        new_letter = 10.times "s"
-      new_letter+string
+     new_letter+string
 
   else
     string
